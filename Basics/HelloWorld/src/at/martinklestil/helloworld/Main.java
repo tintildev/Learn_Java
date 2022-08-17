ef95e4601b3496cd4e6124cf4c2f6b1d32230a77
Martin Klestil <34619286+mklestil@users.noreply.github.com>
2022-08-17 21:21:50 +0200

    Hello World

--- Basics/HelloWorld/src/at/martinklestil/helloworld/Main.java
+++ Basics/HelloWorld/src/at/martinklestil/helloworld/Main.java
@@ -4,5 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        /*
+        public: Zugriffsmodifikator
+        static: Java Schlüsselwort, Methode verwendbar ohne ein Objekt der Klasse zu erstellen.
+        void: Rückgabetyp der Methode, gibt keinen Wert zurück.
+        main: Name der Methode, JVM Einstiegspunkt für die Anwengung
+        String args[]: Parameter die an die main Methode überweben werden.
+        * */
     }
 }
