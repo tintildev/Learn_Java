575ed1cd33d939f1d99dba692bcb4802cc6381e3
Martin Klestil <34619286+mklestil@users.noreply.github.com>
2022-09-12 05:18:47 +0200

    start first JavaFX project
    start first JavaFX project

---
+++ Learn_GUI/JavaFX-Sup/src/sample/Main.java
@@ -0,0 +1,29 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Group root = new Group();
+        Text txt = new Text("Sup? Hello World");
+        txt.setY(50);
+        root.getChildren().add(txt);
+        primaryStage.setTitle("Sup? Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
