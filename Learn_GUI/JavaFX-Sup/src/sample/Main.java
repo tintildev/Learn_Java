24df65bb508b2e92f9475c312b53df9ade838fc0
Martin Klestil <34619286+mklestil@users.noreply.github.com>
2022-09-13 05:24:07 +0200

    add Layout, Btn

--- Learn_GUI/JavaFX-Sup/src/sample/Main.java
+++ Learn_GUI/JavaFX-Sup/src/sample/Main.java
@@ -5,6 +5,8 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
@@ -13,10 +15,22 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+
+        //Container
         Group root = new Group();
-        Text txt = new Text("Sup? Hello World");
+
+        //Add Layout - Vertical Box
+        VBox box = new VBox();
+
+        //Add Nodes
+        Text txt = new Text("Hello World, Sup?");
         txt.setY(50);
-        root.getChildren().add(txt);
+        Button btn = new Button("Say Sup!");
+
+        //add notes to Layout
+        box.getChildren().addAll(txt, btn);
+
+        root.getChildren().add(box);
         primaryStage.setTitle("Sup? Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
