2b4eba42eeac12275cafdf5d21e8d08a58d6c742
Martin Klestil <34619286+mklestil@users.noreply.github.com>
2022-09-15 15:32:05 +0200

    play with javafx

--- Learn_GUI/JavaFX-Sup/src/sample/Main.java
+++ Learn_GUI/JavaFX-Sup/src/sample/Main.java
@@ -6,7 +6,11 @@ import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
@@ -23,12 +27,28 @@ public class Main extends Application {
         VBox box = new VBox();
 
         //Add Nodes
-        Text txt = new Text("Hello World, Sup?");
+        Text txt = new Text("Sup?");
         txt.setY(50);
+        txt.setFont(new Font("Papyrus", 80));
+
+        //Name input
+        Label nameLabel = new Label("Name");
+        TextField nameField = new TextField();
         Button btn = new Button("Say Sup!");
+        GridPane grid = new GridPane();
+        grid.add(nameLabel,0,0);
+        grid.add(nameField,1, 0);
+        grid.add(btn, 1, 1);
+        grid.setHgap(20);
+
+        //see Gridlines
+        //grid.setGridLinesVisible(true);
+
+        //Event
+        btn.setOnAction(event -> System.out.printf("Sup %s!%n", nameField.getText()));
 
         //add notes to Layout
-        box.getChildren().addAll(txt, btn);
+        box.getChildren().addAll(txt, grid);
 
         root.getChildren().add(box);
         primaryStage.setTitle("Sup? Hello World");
