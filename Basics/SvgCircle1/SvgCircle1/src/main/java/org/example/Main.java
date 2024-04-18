650780efc616b7b5d6dde0da68e4f0e14d197f93
Martin Klestil <Martin.klestil@gmx.at>
2024-04-18 11:01:49 +0200

    add SvgCircle
    add SvgCircle projekt

---
+++ Basics/SvgCircle1/SvgCircle1/src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("<svg height='400' width='1000'>");
+        System.out.println("    <circle cx='100' cy='100' r='50'>");
+        System.out.print("</svg>");
+    }
+}
