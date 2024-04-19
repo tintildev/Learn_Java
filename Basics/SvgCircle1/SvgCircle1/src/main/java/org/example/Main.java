66892ceac57bbe2497527e4037fec49fd3bfcd73
Martin Klestil <Martin.klestil@gmx.at>
2024-04-19 10:25:22 +0200

    update ScgCircle1
    add Scanner System.in
    add Math.random

--- Basics/SvgCircle1/SvgCircle1/src/main/java/org/example/Main.java
+++ Basics/SvgCircle1/SvgCircle1/src/main/java/org/example/Main.java
@@ -1,9 +1,22 @@
 package org.example;
 
 public class Main {
+
     public static void main(String[] args) {
+        System.out.println("Bitte x werte eingeben:");
+        int x = new java.util.Scanner(System.in).nextInt();
+        System.out.println("Bitte y werte eingeben:");
+        int y = new java.util.Scanner(System.in).nextInt();
+
+        //Zufallszahl zwischen 10 - 20
+        int range = 20 - 10 + 1;
+        double r = (Math.random() * range ) + 10;
+
+        //Ausgabe für SVG
         System.out.println("<svg height='400' width='1000'>");
-        System.out.println("    <circle cx='100' cy='100' r='50'>");
-        System.out.print("</svg>");
+        System.out.println("    <circle cx='"+ x +"' cy='"+ y +"' r='"+ r +"'>");
+        System.out.println("</svg>");
+
+        System.out.print("Ausgabe eingeben: https://www.w3schools.com/graphics/tryit.asp?filename=trysvg_circle1");
     }
 }
