b3bbc071324ff5a5974468df668364b54e65c774
Martin Klestil <Martin.klestil@gmx.at>
2024-04-22 05:04:04 +0200

    add new project
    work with expressions

---
+++ Basics/workWithExpressions/src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        /*
+            Task:
+            After a raid, the pirates steal bottles and the captain basically gets half.
+            If the number is odd, he gets one less, the crew gets the same share.
+         */
+        System.out.println("Hello world!");
+    }
+}
