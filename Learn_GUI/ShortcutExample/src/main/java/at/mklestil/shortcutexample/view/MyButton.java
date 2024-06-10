1e47e2fe59cd7b45dcd693f5b77ea19460f17174
Martin Klestil <Martin.klestil@gmx.at>
2024-06-10 09:26:52 +0200

    ShortcutExample
    add Image to btn

--- Learn_GUI/ShortcutExample/src/main/java/at/mklestil/shortcutexample/view/MyButton.java
+++ Learn_GUI/ShortcutExample/src/main/java/at/mklestil/shortcutexample/view/MyButton.java
@@ -1,10 +1,32 @@
 package at.mklestil.shortcutexample.view;
 
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 public class MyButton extends Button {
 
+    private String css = "";
+
     public MyButton() {
         this.setPrefSize(64,64);
+    }
+
+    public MyButton(String css, String imgPath){
+        this.css = css;
+        this.getStyleClass().add(this.css);
+        this.setPrefSize(64,64);
+
+        // Add Image
+        ImageView imgView = new ImageView();
+
+        try {
+            Image image = new Image(getClass().getResourceAsStream(imgPath));
+            imgView = new ImageView(image);
+        }catch (Exception e){
+            System.out.println("MyButton: Image Path not found! " + e);
+        }
+
+        this.setGraphic(imgView);
     }
 }
--- Learn_GUI/ShortcutExample/src/main/java/at/mklestil/shortcutexample/view/ShortCutView.java
+++ Learn_GUI/ShortcutExample/src/main/java/at/mklestil/shortcutexample/view/ShortCutView.java
@@ -2,6 +2,8 @@ package at.mklestil.shortcutexample.view;
 
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 
 import java.util.ArrayList;
@@ -12,7 +14,8 @@ public class ShortCutView {
 
     public ShortCutView() {
         ArrayList<Button> btnList = new ArrayList<Button>();
-        btnList.add(new MyButton());
+        MyButton btn1 = new MyButton("btn", "/images/home.png");
+        btnList.add(btn1);
         btnList.add(new MyButton());
         btnList.add(new MyButton());
         btnList.add(new MyButton());
