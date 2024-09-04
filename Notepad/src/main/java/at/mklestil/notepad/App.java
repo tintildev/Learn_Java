f1c36a6479b6233cdf484c754bc5c2494363b001
Martin Klestil <Martin.klestil@gmx.at>
2024-09-04 10:27:06 +0200

    Notepad
    add icon

--- Notepad/src/main/java/at/mklestil/notepad/App.java
+++ Notepad/src/main/java/at/mklestil/notepad/App.java
@@ -4,6 +4,7 @@ import at.mklestil.notepad.control.MyController;
 import at.mklestil.notepad.view.MainWindow;
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -12,12 +13,14 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         // In this Example, i create a notepad.
+        Image icon = new Image(getClass().getResourceAsStream("/images/Notpad.png"));
 
         MainWindow view = new MainWindow();
         MyController controller = new MyController(view);
         Scene scene = new Scene(view.getRoot(), 320, 440);
         stage.setTitle("Notepad");
         stage.setScene(scene);
+        stage.getIcons().add(icon);
         stage.show();
     }
 
